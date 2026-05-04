c57793854835ba7cbf155e467a418e0245379ea3
mirzasalem <mirza.salem2016@gmail.com>
2026-05-04 20:14:25 +0600

    server update for hosting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,8 +6,11 @@ const nextConfig = {
   trailingSlash: true,
   images: { unoptimized: true },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://192.168.68.107:3001",
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://192.168.68.107:3001",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://quran-web-aplication.onrender.com",
   },
 };
 
 module.exports = nextConfig;
+
+
